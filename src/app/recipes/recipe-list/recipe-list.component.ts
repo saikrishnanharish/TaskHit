0a33712865d8ae64ddd7ed02578fd9e3fbc64d0b
saikrishnan <saikrishnan.harish@gmail.com>
2021-08-29 16:51:39 +0530

    to

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,10 @@ import { RecipesModel } from 'src/app/Model/Recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:RecipesModel[]=[
+new RecipesModel('Test Recipe','Test Description','https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?webp=true&quality=90&resize=940%2C399'),
 new RecipesModel('Test Recipe','Test Description','https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?webp=true&quality=90&resize=940%2C399')
-  ]
+
+  ];
 
 
   constructor() { }
